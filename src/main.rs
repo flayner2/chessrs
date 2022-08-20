662a972472c17481943d68799c9b16f5c2659432
flayner2 <flayner5@gmail.com>
2022-08-20 19:15:08 -0300

    Initialized SDL2

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,45 @@
-use bevy::prelude::*;
+use sdl2::{event::Event, pixels::Color};
+
+const WIDTH: u32 = 700;
+const HEIGHT: u32 = 700;
+const BACKGROUND_COLOR: Color = Color::RGB(0, 0, 0);
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
-        .run();
-}
+    let sdl_context = sdl2::init().expect("Failed to initialize SDL2 context");
 
-fn setup(mut commands: Commands) {
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    commands.spawn_bundle(SpriteBundle {
-        sprite: Sprite {
-            color: Color::rgb(0.25, 0.25, 0.75),
-            custom_size: Some(Vec2::new(50.0, 50.0)),
-            ..default()
-        },
-        ..default()
-    });
+    let mut canvas = {
+        let video = sdl_context
+            .video()
+            .expect("Failed to initialize video subsystem");
+
+        let window = video
+            .window("Chessrs", WIDTH, HEIGHT)
+            .position_centered()
+            .build()
+            .expect("Failed to create window");
+
+        window
+            .into_canvas()
+            .accelerated()
+            .present_vsync()
+            .build()
+            .expect("Failed to get render canvas")
+    };
+
+    let mut events = sdl_context.event_pump().expect("Failed to get event loop");
+
+    loop {
+        for event in events.poll_iter() {
+            match event {
+                Event::Quit { .. } => return,
+                _ => {}
+            }
+        }
+
+        canvas.set_draw_color(BACKGROUND_COLOR);
+        canvas.clear();
+
+        // Draw graphics
+        canvas.present();
+    }
 }
